2e2877f89cb09b399e74593011f4f8567b77f971
Sangil Park <65345814+sangil6372@users.noreply.github.com>
2025-03-29 15:48:39 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.15 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,18 +1,6 @@
 class Solution {
     public String solution(String s) {
         String answer = "";
-        StringBuilder sb = new StringBuilder();
-        
-        String[] input = s.toLowerCase().split(" ", -1);
-        
-        for (String str : input){
-            if (!str.isEmpty()){
-                sb.append(Character.toUpperCase(str.charAt(0)));
-                sb.append(str.substring(1));   
-            }
-            sb.append(" ");
-        }
-        
-        return sb.deleteCharAt(sb.length()-1).toString();
+        return answer;
     }
 }
