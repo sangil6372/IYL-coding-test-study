329ca90499bf5831f2fc87e4559dc1ea00643c76
Sangil Park <65345814+sangil6372@users.noreply.github.com>
2025-03-30 15:51:27 +0900

    [level 2] Title: 구명보트, Time: 11.31 ms, Memory: 58.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        
+        Arrays.sort(people);
+        
+        int start = 0;
+        int end = people.length-1;
+        
+        while (start <= end){
+            if (people[start] + people[end] > limit ){
+                end--;
+            }else{
+                start++;
+                end--;
+            }
+            answer++;
+        }
+        
+        return answer;
+    }
+}
