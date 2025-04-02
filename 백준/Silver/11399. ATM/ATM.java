98e29d53d5c2e099ffde7e9dbc85e0984a960c90
Sangil Park <65345814+sangil6372@users.noreply.github.com>
2025-04-02 23:12:44 +0900

    [Silver IV] Title: ATM, Time: 124 ms, Memory: 14512 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,32 +1,31 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
-
-		int N = Integer.parseInt(bReader.readLine());
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		StringTokenizer st = new StringTokenizer(bReader.readLine());
-		int[] arr = new int[N];
+		int N = Integer.parseInt(br.readLine());
 
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
+		String[] input = br.readLine().split(" ");
 
-		Arrays.sort(arr);
-
-		int result = 0;
-
-		for (int i = 0; i < N; i++) {
-			result += arr[i]*(N-i);
+		List<Integer> list = new ArrayList<>();
+		for (String s : input) {
+			list.add(Integer.parseInt(s));
 		}
 		
-		System.out.println(result);
+		Collections.sort(list);
+
+//		(N-순번) 만큼 자신의 이용 시간이 누적됨 ㅇㅇ <- (N-i)*arr[i번째]
+		
+//		쉽게 말하면 친구들끼리 여행가서 씻을 때 빨리 씻는 사람 먼저 씻는 느낌
+//		근데 씻는 사람을 기다리는 사람들은 다음 사람만 해당 되는게 아니라 모든 아직 안 씻은 사람들임
+//		총 소요 시간이라 본인 씻는 시간까지 고려 (N-i-1)가 아니라  (N-i)본인 포함
+		int answer = 0;
+		for (int i = 0 ; i <input.length; i++ ) {
+			answer += (N-i)*list.get(i);
+		}
+
+		System.out.println(answer);
 	}
 }
