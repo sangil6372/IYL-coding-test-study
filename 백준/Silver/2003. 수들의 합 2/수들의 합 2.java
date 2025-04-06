ef720970db62912bd319666941eebc0f51661236
Sangil Park <65345814+sangil6372@users.noreply.github.com>
2025-04-07 00:08:09 +0900

    [Silver IV] Title: 수들의 합 2, Time: 128 ms, Memory: 15580 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -1,4 +1,4 @@
-import java.io.*;
+ import java.io.*;
 
 public class Main {
 	public static void main(String args[]) throws IOException {
@@ -13,32 +13,23 @@ public class Main {
 		for (int i = 0; i< input.length; i++) {
 			arr[i] = Integer.parseInt(input[i]);
 		}
-		// 아래에서부터 핵심 로직 
-		int count = 0;
-		
-//		for (int i = 0 ; i < N; i++) {
-//			int sum = 0;
-//			for (int j = i; j < arr.length; j++) {
-//				sum += arr[j];
-//				if (sum == M) count++; 
-//			}
-//		}
+		// 아래에서부터 핵심 로직 		
 		int startIdx = 0;
-		int endIdx = 0;
+		int endIdx = 0; // endIdx 는 다음에 더할 위치!!
 		int sum = 0;
-		while (endIdx < N || sum >= M) { // 종료하기 전에 기회를 줌
+		int count = 0;
+		while (endIdx < N+1) { // 종료하기 전에 기회를 줌
 			if (sum < M) {
-		        sum += arr[endIdx++];
+		        sum += arr[endIdx++]; // 현재 위치를 더하고 다음으로 end이동
 			}
 			else if (sum > M) {
-				sum -= arr[startIdx++];
+						sum -= arr[startIdx++];  // 현재 위치를 빼고 다음으로 start 이동
 			}
 			else {  // (sum == M)
 		        count++;
-				sum -= arr[startIdx++];
+						sum -= arr[startIdx++]; // end를 증가 시키면 범위를 벗어남! 
 		    } 
 		}
-		
 		System.out.println(count);
 	}
 }
