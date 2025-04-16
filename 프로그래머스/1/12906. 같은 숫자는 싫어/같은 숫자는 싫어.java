630dbfa1ad6b82ac83f2463ec627a87eb28ed1e5
Sangil Park <65345814+sangil6372@users.noreply.github.com>
2025-04-16 17:24:46 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 50.59 ms, Memory: 117 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        
+        Deque<Integer> dQ = new ArrayDeque<>();
+        dQ.add(arr[0]);
+        
+        for (int i = 1; i<arr.length;i++){
+            if (dQ.getLast()==arr[i]) continue;
+            dQ.addLast(arr[i]);
+        }
+        int length = dQ.size();
+        int[] answer = new int[length];
+        
+        for (int i = 0; i<length;i++){
+            answer[i] = dQ.poll();
+        }
+        
+        
+        return answer;
+    }
+}
