1bed57327189534851c8f74026d9258241c194b8
Sangil Park <65345814+sangil6372@users.noreply.github.com>
2025-05-11 20:04:36 +0900

    [Gold IV] Title: 최단경로, Time: 692 ms, Memory: 107832 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,106 +1,80 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    static class Node implements Comparable<Node> {
+        int to, weight;
 
-	static int[][] graph;
-	static final int INF = 9999999;
-	static int V;
-	static int[] dist;
-	static ArrayList<ArrayList<Node>> arr;
+        Node(int to, int weight) {
+            this.to = to;
+            this.weight = weight;
+        }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		
-		V = Integer.parseInt(st.nextToken()); // 정점 개수
-		int E = Integer.parseInt(st.nextToken()); // 간선 개수
+        @Override
+        public int compareTo(Node other) {
+            return Integer.compare(this.weight, other.weight);
+        }
+    }
 
-		int K = Integer.parseInt(br.readLine()); // 시작 정점
+    static final int INF = Integer.MAX_VALUE;
+    static List<Node>[] graph;
+    static int[] dist;
 
-		
-		dist = new int[V + 1]; // K에서 부터 거리 갱신하며 저장
-		Arrays.fill(dist, INF);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-		arr = new ArrayList<>();
-		
-		for (int i = 0;i<V+1;i++) {
-			arr.add(new ArrayList<>());
-		}
-		
-		
-		while (E-- > 0) {
-			st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
 
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(br.readLine());
+ 
+        graph = new ArrayList[V + 1];
+        dist = new int[V + 1];
+        Arrays.fill(dist, INF);
 
-			arr.get(u).add(new Node(v, w));
-		}
+        for (int i = 1; i <= V; i++) {
+            graph[i] = new ArrayList<>();
+        }
 
-		dijstra(K);
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            graph[u].add(new Node(v, w));
+        }
 
-		for (int i = 1; i <= V; i++) {
-			if (dist[i] == INF) {
-				sb.append("INF\n");
-				continue;
-			}
-			sb.append(dist[i] + "\n");
-		}
+        dijkstra(K);
 
-		System.out.println(sb);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= V; i++) {
+            if (dist[i] == INF) {
+                sb.append("INF\n");
+            } else {
+                sb.append(dist[i]).append("\n");
+            }
+        }
+        System.out.print(sb);
+    }
 
-	}
+    static void dijkstra(int start) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        dist[start] = 0;
+        pq.offer(new Node(start, 0));
 
-	public static class Node {
-		int end;
-		int weight;
+        while (!pq.isEmpty()) {
+            Node cur = pq.poll();
 
-		public Node(int end, int weight) {
-			this.end = end;
-			this.weight = weight;
-		}
+            if (cur.weight > dist[cur.to]) continue;
 
-	}
-
-	public static void dijstra(int start) {
-
-		PriorityQueue<Node> pQueue = new PriorityQueue<>(new Comparator<Node>() {
-
-			@Override
-			public int compare(Node o1, Node o2) {
-				return o1.weight - o2.weight;
-			}
-		});
-
-		pQueue.add(new Node(start, 0));
-		dist[start] = 0;
-
-		while (!pQueue.isEmpty()) {
-
-			Node curNode = pQueue.poll();
-
-			if (curNode.weight>dist[curNode.end]) continue;
-
-			for (Node n : arr.get(curNode.end)){
-				if (dist[n.end] > curNode.weight + n.weight) {
-					dist[n.end] = curNode.weight +n.weight;
-					pQueue.add(new Node(n.end, dist[n.end]));
-				}
-			}
-			
-		
-		}
-
-	}
-
+            for (Node next : graph[cur.to]) {
+                if (dist[next.to] > dist[cur.to] + next.weight) {
+                    dist[next.to] = dist[cur.to] + next.weight;
+                    pq.offer(new Node(next.to, dist[next.to]));
+                }
+            }
+        }
+    }
 }
