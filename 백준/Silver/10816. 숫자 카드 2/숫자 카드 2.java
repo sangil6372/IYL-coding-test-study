d1f7c6ce6dd9899802490c687f41da6a5ad3e166
Sangil Park <65345814+sangil6372@users.noreply.github.com>
2025-05-27 18:50:43 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1288 ms, Memory: 188728 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		// 검은 방 1 흰방 0 가중치를 부여
+		// 최대로 많은 흰방을 거치면..? 결과가 같아지는 거 아닌가?
+		int N = Integer.parseInt(br.readLine());
+		Map<Integer, Integer> map = new HashMap<>();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			map.put(num, map.getOrDefault(num, 0) + 1);
+		}
+		int size = map.size();
+		
+		int[] arr = new int[size];
+		
+		int i = 0;
+		for (int key : map.keySet()) {
+			arr[i] = map.get(key);
+			i++;
+		}
+		Arrays.sort(arr);
+		
+		int M = Integer.parseInt(br.readLine());
+		
+		st = new StringTokenizer(br.readLine());
+		while(M-->0) {
+			int key  = Integer.parseInt(st.nextToken());
+			sb.append(map.getOrDefault(key, 0)+ " ");
+		}
+		System.out.println(sb);
+
+	}
+
+	public static boolean binarySearch(int[] arr, int key) {
+		int start = 0;
+		int end = arr.length - 1;
+
+		while (start <= end) {
+			int mid = (end + start) / 2;
+
+			if (key < arr[mid]) {
+				end = mid - 1;
+			} else if (key > arr[mid]) {
+				start = mid + 1;
+
+			} else
+				return true;
+		}
+		return false;
+	}
+
+}
