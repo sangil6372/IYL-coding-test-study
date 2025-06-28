dab004bd37fb02b37a6435319bdc201cdce6970d
Sangil Park <65345814+sangil6372@users.noreply.github.com>
2025-06-28 15:37:09 +0900

    [Gold III] Title: 선분 교차 1, Time: 104 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Gold/17386. 선분 교차 1/선분 교차 1.java
+++ 백준/Gold/17386. 선분 교차 1/선분 교차 1.java
@@ -1,35 +1,43 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
-		st = new StringTokenizer(br.readLine());
-		int x1 = Integer.parseInt(st.nextToken());
-		int y1 = Integer.parseInt(st.nextToken());
-		int x2 = Integer.parseInt(st.nextToken());
-		int y2 = Integer.parseInt(st.nextToken());
-		st = new StringTokenizer(br.readLine());
-		int x3 = Integer.parseInt(st.nextToken());
-		int y3 = Integer.parseInt(st.nextToken());
-		int x4 = Integer.parseInt(st.nextToken());
-		int y4 = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        long x1 = Long.parseLong(st.nextToken());
+        long y1 = Long.parseLong(st.nextToken());
+        long x2 = Long.parseLong(st.nextToken());
+        long y2 = Long.parseLong(st.nextToken());
 
-		char result = '0';
-		if (ccw(x1, y1, x2, y2, x3, y3) * ccw(x1, y1, x2, y2, x4, y4) < 0 && 
-	            ccw(x3, y3, x4, y4, x1, y1) * ccw(x3, y3, x4, y4, x2, y2) < 0) result = '1';
+        st = new StringTokenizer(br.readLine());
+        long x3 = Long.parseLong(st.nextToken());
+        long y3 = Long.parseLong(st.nextToken());
+        long x4 = Long.parseLong(st.nextToken());
+        long y4 = Long.parseLong(st.nextToken());
 
-		bw.write(result + "\n");
+        int a = ccw(x1, y1, x2, y2, x3, y3);
+        int b = ccw(x1, y1, x2, y2, x4, y4);
+        int c = ccw(x3, y3, x4, y4, x1, y1);
+        int d = ccw(x3, y3, x4, y4, x2, y2);
 
-		bw.flush();
-		bw.close();
-		br.close();
-	}
+        if (a * b < 0 && c * d < 0) {
+            bw.write("1\n");
+        } else {
+            bw.write("0\n");
+        }
 
-	public static int ccw(long x1, long y1, long x2, long y2, long x3, long y3) {
-		return x1 * y2 + x2 * y3 + x3 * y1 - y1 * x2 - y2 * x3 - y3 * x1 < 0 ? 1 : -1;
-	}
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    // ccw가 무조건 -1 (반시계) 혹은 1 (시계) 반환
+    public static int ccw(long x1, long y1, long x2, long y2, long x3, long y3) {
+        long result = (x1 * y2 + x2 * y3 + x3 * y1) - (y1 * x2 + y2 * x3 + y3 * x1);
+        return result < 0 ? 1 : -1;
+    }
 }
