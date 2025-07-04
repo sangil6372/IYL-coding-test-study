ed707cad207f6ccee62106e84c10dc9794426e09
Sangil Park <65345814+sangil6372@users.noreply.github.com>
2025-07-04 13:14:18 +0900

    [level 2] Title: 타겟 넘버, Time: 5.06 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -2,34 +2,19 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] numbers, int target) {
-        int answer = bfs(numbers, target);
+        int answer = dfs(numbers, target, 0, 0);
         return answer;
     }
 
-    public static int bfs(int[] numbers, int target){
-        
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(0);
-        int count = 0;
-        int depth = 0;
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            for (int i = 0 ; i < size ; i++){
-                int curState = queue.poll();   
-                if (depth == numbers.length){
-                    if (curState == target) {
-                        count++;
-                        continue;
-                    }
-                }
-                else {
-                    queue.add(curState+numbers[depth]);
-                    queue.add(curState-numbers[depth]);
-                }
-            }
-            depth++;
+    public int dfs(int[] numbers , int target , int idx , int curr){
+        if (idx == numbers.length){
+            return (curr==target) ? 1 : 0 ;
         }
-        return count;
+        int sum = 0;
+        sum += dfs(numbers, target, idx+1, curr+numbers[idx]);
+        sum += dfs(numbers, target, idx+1, curr-numbers[idx]);
+        
+        return sum;
     }
     
 }
